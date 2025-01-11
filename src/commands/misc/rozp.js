0a41efd99e552fe0ae10440633f3778f0484be32
Kamper <57861774+Kamperrro@users.noreply.github.com>
2025-01-11 10:29:48 +0100

    Fuzzy search do nicków

--- src/commands/misc/rozp.js
+++ src/commands/misc/rozp.js
@@ -1,34 +1,52 @@
 const { SlashCommandBuilder, EmbedBuilder, WebhookClient } = require('discord.js');
 const { exec } = require('child_process');
 const excludedWords = require('../../config/excluded-words.json');
+const playerSample = require('../../config/players.json');
+const Fuse = require('fuse.js');
 require('dotenv').config();
 
 module.exports = {
 	data: new SlashCommandBuilder().setName('rozpoznaj').setDescription('Do przycisków'),
 	async execute(interaction, client) {
-		let input = 'siema generlanie frajer z nickiem osamabinladen spawn skibdi frajer_jebany sigmeuszkilluje smiec jebany xddd TK TK TK !!!!';
-		input = input.toLowerCase();
-		input = input.replace(/[^a-zA-Z0-9ąćęłńóśźżĄĆĘŁŃÓŚŹŻ ]/g, '');
-		input = input.split(' ').filter((word) => !excludedWords.exclude.includes(word));
-		console.log(input);
+		const inputRaw = 'osama';
+		let input = inputRaw.replace(/\[.*?\]/g, ''); // kasowanie tagów klanowych
+		input = input.replace(/[^a-zA-Z0-9ąćęłńóśźżĄĆĘŁŃÓŚŹŻ ]/g, ''); // kasowanie znaków specjalnych
+		input = input.split(/\s+/); // rozdzielenie na słowa
 
-		let matchingPlayers = [];
+		let list = [];
+		playerSample.result.forEach((player) => {
+			list.push({
+				name: player[0],
+				nameParsed: player[0]
+					.replace(/\[.*?\]/g, '') // kasowanie tagów klanowych
+					.replace(/[^a-zA-Z0-9ąćęłńóśźżĄĆĘŁŃÓŚŹŻ ]/g, '') // kasowanie znaków specjalnych
+					.split(/\s+/),
+				id: player[1],
+			});
+		});
 
-		const playerNicknames = ['osamabinladen tk', 'skibidi sigmeusz', 'frajer_jebany'];
-		for (const player of playerNicknames) {
-			const lowerPlayer = player.toLowerCase();
-			for (const potential of lowerPlayer.split(' ').filter((word) => !excludedWords.exclude.includes(word))) {
-				if (input.includes(potential)) {
-					input = input.map((word) => (word === potential ? player : word));
-					matchingPlayers.push(player);
-				}
-			}
+		const options = {
+			includeScore: true,
+			keys: ['nameParsed'],
+			minMatchCharLength: 3,
+			threshold: 0.5,
+		};
+		const fuse = new Fuse(list, options);
+
+		let results = [];
+		for (const word of input) {
+			if (word.length < 4) continue;
+			results = [...results, ...fuse.search(word)];
 		}
-		console.log(matchingPlayers);
+
+		results.sort((a, b) => a.score - b.score);
+		// const result = fuse.search(inputRaw);
+		const best = results[0];
+		console.log(best);
 
 		await interaction.reply({
 			ephemeral: true,
-			content: input.join(' '),
+			content: `Najbardziej pasuje: ${best.item.name} (${best.item.id})`,
 		});
 	},
 };
