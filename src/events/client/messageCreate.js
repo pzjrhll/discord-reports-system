d50f0e17f2834b6485ae195c5a5f857ea97d6d3f
Kamper <57861774+Kamperrro@users.noreply.github.com>
2025-01-11 23:22:26 +0100

    Update messageCreate.js

--- src/events/client/messageCreate.js
+++ src/events/client/messageCreate.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, WebhookClient } = require('discord.js');
-const { proccessReport } = require('../../functions/utils/reportWebhook.js');
+const { processReport } = require('../../functions/utils/reportSystem.js');
 require('dotenv').config();
 
 module.exports = {
@@ -10,7 +10,7 @@ module.exports = {
 			// if (message.author.bot) return;
 
 			if (message.channel.id === process.env.REPORTS_WEBHOOK_CHANNEL_ID && message.author.bot && message.author.id !== process.env.DISCORD_CLIENT_ID) {
-				return proccessReport(message, client);
+				return processReport(message, client);
 			}
 		} catch (err) {
 			client.cerr(err);
