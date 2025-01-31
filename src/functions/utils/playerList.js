88115659ea3f85f197d1fb49643651f62634d221
Kamper <57861774+Kamperrro@users.noreply.github.com>
2025-01-31 19:10:33 +0100

    Update playerList.js

--- src/functions/utils/playerList.js
+++ src/functions/utils/playerList.js
@@ -1,10 +1,10 @@
-const playerSample = require('../../config/players.json');
-const playersSampleFull = require('../../config/playersFull.json');
 require('dotenv').config();
+const { getPlayerIds, getPlayerDetailedInfo } = require('./apiWrapper');
 
-async function getPlayers() {
+async function parsePlayerList(serverId) {
 	let list = [];
-	playerSample?.result?.forEach((player) => {
+	const data = await getPlayerIds(serverId);
+	data?.result?.forEach((player) => {
 		list.push({
 			name: player[0],
 			id: player[1],
@@ -13,6 +13,28 @@ async function getPlayers() {
 	return list;
 }
 
-async function getPlayersFull() {}
+async function parsePlayerInfo(serverid, playername) {
+	const fetch = await getPlayerDetailedInfo(serverid, playername);
+	const player = fetch?.result;
+	if (!player) return null;
 
-module.exports = { getPlayers, getPlayersFull };
+	const totalPlaytimeSeconds = player?.profile?.total_playtime_seconds || 0;
+	const hours = Math.floor(totalPlaytimeSeconds / 3600);
+	const minutes = Math.floor((totalPlaytimeSeconds % 3600) / 60);
+	const penalties = player?.profile?.penalty_count;
+
+	let playerInfo = {
+		name: player?.name,
+		player_id: player?.player_id,
+		squad: `${player?.team} | ${player?.unit_name} | ${player?.role}`,
+		playtime: `${hours}h ${minutes}m | ${player?.profile?.sessions_count}`,
+		vip: player?.is_vip ? 'Tak' : 'Nie',
+		watchlist: player?.profile?.watchlist || '-',
+		penalty_count: `Ban: ${penalties?.PERMABAN + penalties?.TEMPBAN} | Kick: ${penalties?.KICK}`,
+		stats: `WIP`,
+		series: `WIP`,
+	};
+	return playerInfo;
+}
+
+module.exports = { parsePlayerList, parsePlayerInfo };
