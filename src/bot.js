3f67a9ee2a2b207800cf256d9c18855a62655b4b
Kamper <57861774+Kamperrro@users.noreply.github.com>
2025-02-01 00:51:51 +0100

    Removed unnecessary comments

--- src/bot.js
+++ src/bot.js
@@ -17,44 +17,6 @@ const client = new Client({
 
 const localConfig = JSON.parse(fs.readFileSync('./src/config/local.json', 'utf-8'));
 
-// ------ SQL Function ------
-// const pool = mysql.createPool({
-// 	connectionLimit: 5,
-// 	host: process.env.SQL_AUTH_IP,
-// 	user: process.env.SQL_AUTH_USERNAME,
-// 	password: process.env.SQL_AUTH_PASSWORD,
-// 	database: process.env.SQL_DATABASE_NAME,
-// });
-
-// pool.getConnection((err, connection) => {
-// 	if (err) throw err;
-// 	console.log(`[MYSQL] Connected to database successfully!`);
-// 	connection.release();
-// });
-// client.pool = pool;
-
-// client.config = async () => {
-// 	let sqlres = await client.pool.promise().query('SELECT * FROM sp_config');
-// 	sqlres = sqlres[0];
-// 	let res = {};
-
-// 	for (const entry of sqlres) {
-// 		try {
-// 			const value = JSON.parse(entry.config_value);
-// 			const key = entry.config_key;
-
-// 			res[key] = value;
-// 		} catch (err) {
-// 			console.log('---------- ERROR ----------');
-// 			console.log(entry.config_key);
-// 			console.log(entry.config_value);
-// 			console.log(err);
-// 		}
-// 	}
-
-// 	return res;
-// };
-
 client.config = () => {
 	let obj = localConfig;
 	obj.logsAllWebhook = process.env.LOGS_WEBHOOK_URL;
@@ -69,11 +31,4 @@ for (const folder of functionFolders) {
 }
 
 client.handleEvents();
-// client.on('debug', (e) => {
-// 	console.warn(e);
-// 	console.log('xdddddd');
-// });
-
 client.login(process.env.DISCORD_BOT_TOKEN);
-// console.log(chalk())
-// process.exit(0);
