82c4ce56272e18ec426fa04f1e6019550ef2f536
Kamper <57861774+Kamperrro@users.noreply.github.com>
2025-02-01 12:46:50 +0100

    Remove unused imports

--- src/components/buttons/report-close.js
+++ src/components/buttons/report-close.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, WebhookClient } = require('discord.js');
+const { EmbedBuilder } = require('discord.js');
 require('dotenv').config();
 
 module.exports = {
@@ -10,11 +10,6 @@ module.exports = {
 		await interaction.deferReply({
 			fetchReply: true,
 			ephemeral: true,
-		});
-		const config = await client.config();
-
-		const webhook = new WebhookClient({
-			url: process.env.REPORTS_WEBHOOK_URL,
 		});
 		const triggerMsg = await interaction.channel.messages.fetch(actionId);
 		const embedData = triggerMsg?.embeds[0]?.data;
